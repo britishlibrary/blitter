912ad2806430e0d4eb7e7b7d9e96b4bb96ed6dc1
Andrew Jackson <Andrew.Jackson@bl.uk>
2016-12-05 22:23:40 +0000

    Split into two steps, so we don't have to go back to the download each time.

--- streaming/tasks.py
+++ streaming/tasks.py
@@ -11,13 +11,25 @@ import genblit
 
 logger = logging.getLogger('luigi-interface')
 
+
+class blit(luigi.Config):
+    """
+    Configuration class for these tasks
+
+    Parameters:
+        url_template = String template to construct a URL from an identifier.
+
+    """
+    url_template = luigi.Parameter(default="http://nellie-private.bl.uk:14000/webhdfs/v1/user/anjackson/%s?op=OPEN&user.name=hdfs")
+
+
 class ExternalListFile(luigi.ExternalTask):
-    input_file = luigi.Parameter()
     """
     Example of a possible external data dump
     To depend on external targets (typically at the top of your dependency graph), you can define
     an ExternalTask like this.
     """
+    input_file = luigi.Parameter()
 
     def output(self):
         """
@@ -29,64 +41,142 @@ class ExternalListFile(luigi.ExternalTask):
         return luigi.contrib.hdfs.HdfsTarget(self.input_file)
 
 
-class GenerateBlit(luigi.contrib.hadoop.JobTask):
+class RunJpylyzer(luigi.contrib.hadoop.JobTask):
+    """
+    This class takes a list of identifiers for JPEG2000 files, then downloads them, and then runs Jpylyzer on them
+    to extract metadata about the file.
+
+    The output is of the form:
+
+        <identifier><tab><jpylyzer-xml-as-string><newline>
+        ....
+
+    Parameters:
+        input_file: The file (on HDFS) that contains the list of identifiers.
+    """
     input_file = luigi.Parameter()
 
     def output(self):
-        return luigi.contrib.hdfs.HdfsTarget("data/blit.tsv")
+        return luigi.contrib.hdfs.HdfsTarget("blitter/jpylyzer.tsv")
 
     def requires(self):
         return ExternalListFile(self.input_file)
 
     def extra_modules(self):
-        return [jpylyzer, genblit]
+        return [jpylyzer]
 
     def mapper(self, line):
-        '''
+        """
         Each line should be an identifier of a JP2 file, e.g. 'vdc_100022551931.0x000001'
 
-        In the mapper we download, and then jpylyze it, then convert to blit for output.
+        In the mapper we download, and then jpylyze it
 
         :param line:
         :return:
-        '''
-
+        """
 
         # Ignore blank lines:
         if line == '':
             return
 
         try:
-            # Download to temp file:
+            # Create to temp file:
             (jp2_fd, jp2_file) = tempfile.mkstemp()
-            #download_url = \
-            #    "https://github.com/anjackson/blitter/blob/master/jython/src/test/resources/test-data/%s?raw=true" % line
-            download_url = "http://nellie-private.bl.uk:14000/webhdfs/v1/user/anjackson/%s?op=OPEN&user.name=hdfs" % line
-            logger.warning(download_url)
+
+            # Construct URL and download:
+            download_url = blit().url_template % line
+            logger.warning("Downloading: %s " % download_url)
             (tempfilename, headers) = urllib.urlretrieve(download_url, jp2_file)
 
             # Jpylyser-it:
             jpylyzer_xml = jpylyzer.checkOneFile(jp2_file)
 
-            # Convert to blit xml:
-            blit_xml = genblit.to_blit(jpylyzer_xml)
-
             # Map to a string, and strip out newlines:
-            xml_out = ET.tostring(blit_xml, 'UTF-8', 'xml')
-            xml_out = xml_out.replace('\n','')
-
-            logger.info(xml_out)
+            jpylyzer_xml_out = ET.tostring(jpylyzer_xml, 'UTF-8', 'xml')
+            jpylyzer_xml_out = jpylyzer_xml_out.replace('\n','')
 
             # Delete the temp file:
             os.remove(jp2_file)
         except Exception as e:
-            xml_out = "FAILED! %s" % e
+            jpylyzer_xml_out = "FAILED! %s" % e
 
         # And return:
-        yield line, xml_out
+        yield line, jpylyzer_xml_out
 
     def reducer(self, key, values):
-        # Pass-through reducer:
+        """
+        A pass-through reducer.
+
+        :param key:
+        :param values:
+        :return:
+        """
+        for value in values:
+            yield key, value
+        # An actual reducer:
+        #yield key, sum(values)
+
+
+class GenerateBlit(luigi.contrib.hadoop.JobTask):
+    """
+    This class takes the output from Jpylyzer and transforms it into 'blit' XML.
+
+    """
+    input_file = luigi.Parameter()
+
+    def requires(self):
+        return RunJpylyzer(self.input_file)
+
+    def output(self):
+        return luigi.contrib.hdfs.HdfsTarget("data/blit.tsv")
+
+    def extra_modules(self):
+        return [genblit]
+
+    def mapper(self, line):
+        """
+        Each line should be an identifier of a JP2 file, e.g. 'vdc_100022551931.0x000001' followed by a string
+        that is the XML output from Jpylyzer.
+
+        In the mapper we re-parse, then convert to blit for output.
+
+        :param line:
+        :return:
+        """
+
+        # Ignore blank lines:
+        if line == '':
+            return
+
+        try:
+            # Split the input:
+            (id, jpylyzer_xml_out) = line.split('\t', 1)
+
+            # Re-parse the XML:
+            jpylyzer_xml = ET.fromstring(jpylyzer_xml_out)
+
+            # Convert to blit xml:
+            blit_xml = genblit.to_blit(jpylyzer_xml)
+
+            # Map to a string, and strip out newlines:
+            blit_xml_out = ET.tostring(blit_xml, 'UTF-8', 'xml')
+            blit_xml_out = blit_xml_out.replace('\n','')
+
+        except Exception as e:
+            blit_xml_out = "FAILED! %s" % e
+
+        # And return both forms:
+        yield line, blit_xml_out
+
+    def reducer(self, key, values):
+        """
+        A pass-through reducer.
+
+        :param key:
+        :param values:
+        :return:
+        """
+
         for value in values:
             yield key, value
         # An actual reducer:
